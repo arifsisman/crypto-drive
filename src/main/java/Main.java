45c4a53174f1ef17062ec621a01e4c94edebf232
Mustafa Arif Sisman <mustafaarifsisman@gmail.com>
2018-12-04 01:42:02 +0300

    Added relative path for file.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,7 +10,7 @@ public class Main {
     public static void main(String[] args) throws IOException, GeneralSecurityException {
         com.google.api.services.drive.Drive service = DriveInit();
         List.listFiles(service);
-        String filePath = "C:\\Users\\musta\\IdeaProjects\\CryptoDrive\\src\\main\\resources\\files\\photo.jpg";
+        String filePath = "src/main/resources/files/photo.jpg";
         Upload.withPath(filePath);
     }
 }
