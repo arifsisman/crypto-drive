c671092f11a178c6a68aa942e2e355bce92152e7
Mustafa Arif Sisman <mustafaarifsisman@gmail.com>
2018-12-16 08:24:05 +0300

    DirWatcher and listener thread added.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,14 @@
 import Crypto.CipherOps;
 import Drive.DriveService;
-import Monitor.MonitorDirectory;
+import Monitor.DirWatcher;
 
 import java.io.IOException;
+import java.nio.file.Path;
 import java.security.GeneralSecurityException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author Mustafa Sisman
@@ -14,16 +19,42 @@ public class Main {
         //Drive Service initialization
         DriveService.initialize();
 
+        Thread t = new Thread() {
+            public void run() {
+                DirWatcher watcher = null;
+                try {
+                    watcher = new DirWatcher(Path.of("C:\\Users\\musta\\Documents\\CryptoDrive"));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                ExecutorService executor = Executors.newSingleThreadExecutor();
+                Future<?> future = executor.submit(watcher);
+                executor.shutdown();
+            }
+        };
+        t.start();
+
         //Cipher initialize for encryption/decryption operations
         CipherOps cipher = new CipherOps();
         cipher.encrypt("C:\\Users\\musta\\IdeaProjects\\CryptoDrive\\src\\main\\resources\\files\\photo.jpg");
         cipher.decrypt("C:\\Users\\musta\\IdeaProjects\\CryptoDrive\\src\\main\\resources\\files\\photo.jpg.enc");
 
+        //System.out.println("operation completed");
+
+        // Now, the watcher runs in parallel
+        // Do other stuff here
+
+//        // Shutdown after 10 seconds
+//        executor.awaitTermination(10, TimeUnit.SECONDS);
+//        // abort watcher
+//        future.cancel(true);
+//
+//        executor.awaitTermination(1, TimeUnit.SECONDS);
+//        executor.shutdownNow();
+
 
         //Monitor the directory for changes
-        MonitorDirectory.listen();
-
-
+        //MonitorDirectory.listen();
 
         //Search.searchFilesByName("", DriveService.folderId);
         //TextMenu menu = new TextMenu();
---
+++ src/main/java/Monitor/DirWatcher.java
@@ -0,0 +1,66 @@
+package Monitor;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.sql.Time;
+import java.time.LocalTime;
+
+import static java.nio.file.StandardWatchEventKinds.*;
+
+public class DirWatcher implements Runnable {
+
+    private final Path dir;
+    private final WatchService watcher;
+    private final WatchKey key;
+
+    @SuppressWarnings("unchecked")
+    static <T> WatchEvent<T> cast(WatchEvent<?> event) {
+        return (WatchEvent<T>) event;
+    }
+
+    /**
+     * Creates a WatchService and registers the given directory
+     */
+    public DirWatcher(Path dir) throws IOException {
+        this.dir = dir;
+        this.watcher = FileSystems.getDefault().newWatchService();
+        this.key = dir.register(watcher, ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY);
+    }
+
+    public void run() {
+        try {
+            for (;;) {
+                // wait for key to be signalled
+                WatchKey key = watcher.take();
+
+                if (this.key != key) {
+                    System.err.println("WatchKey not recognized!");
+                    continue;
+                }
+
+                for (WatchEvent<?> event : key.pollEvents()) {
+                    WatchEvent<Path> ev = cast(event);
+                    System.out.format("%s: %s\n", ev.kind(), dir.resolve(ev.context()));
+                    // TODO: handle event. E.g. call listeners
+                    WatchEvent.Kind kind = event.kind();
+                    if (StandardWatchEventKinds.ENTRY_CREATE.equals(kind)){
+
+                    }
+                    if (StandardWatchEventKinds.ENTRY_DELETE.equals(kind)){
+
+                    }
+                    if (StandardWatchEventKinds.ENTRY_MODIFY.equals(kind)){
+
+                    }
+                }
+
+                // reset key
+                if (!key.reset()) {
+                    break;
+                }
+            }
+        } catch (InterruptedException x) {
+            return;
+        }
+    }
+}
